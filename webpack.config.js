182b3b00da36e5a2c3bb1ea09bef0faa8d84f15f
Rafael <rafael_guliev94@list.ru>
2020-05-01 23:24:04 +0300

    master | init

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new CleanWebpackPlugin()
+    ]
+};
