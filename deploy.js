070e7a1fdbdbe551bf356d53e615345dffec7c1c
Rafael <rafael_guliev94@list.ru>
2020-07-08 13:05:11 +0300

    master | добавил пакет gh-pages

--- deploy.js
+++ deploy.js
@@ -1,5 +1,5 @@
 const ghpages = require('gh-pages');
-const repositoryUrl = ''; // Адрес репозитория, в который вы хотите сделать деплой, например: https://github.com/likeavenus/portfolio
+const repositoryUrl = 'https://github.com/likeavenus/webpack-build'; // Адрес репозитория, в который вы хотите сделать деплой, например: https://github.com/likeavenus/portfolio
 
 ghpages.publish('dist', {
     branch: 'gh-pages',
