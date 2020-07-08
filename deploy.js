e8a585f8f13a6f68f4cc9cfaa98d16a9297f633b
Rafael <rafael_guliev94@list.ru>
2020-07-08 12:57:47 +0300

    master | добавил деплой

--- deploy.js
+++ deploy.js
@@ -1,5 +1,5 @@
 const ghpages = require('gh-pages');
-const repositoryUrl = ''; // Адрес репозитория, в который вы хотите сделать деплой
+const repositoryUrl = ''; // Адрес репозитория, в который вы хотите сделать деплой, например: https://github.com/likeavenus/portfolio
 
 ghpages.publish('dist', {
     branch: 'gh-pages',
