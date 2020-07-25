145b1f738b0f72a710e674392d79c98ef2782150
Rafael <37466798+likeavenus@users.noreply.github.com>
2020-07-26 00:24:37 +0300

    master | mini refactor deploy.js

--- deploy.js
+++ deploy.js
@@ -1,5 +1,6 @@
 const ghpages = require('gh-pages');
-const repositoryUrl = 'https://github.com/likeavenus/webpack-build'; // Адрес репозитория, в который вы хотите сделать деплой, например: https://github.com/likeavenus/portfolio
+const NAME = ''; // repo name
+const repositoryUrl = `https://github.com/likeavenus/${NAME}`; // Адрес репозитория, в который вы хотите сделать деплой, например: https://github.com/likeavenus/portfolio
 
 ghpages.publish('dist', {
     branch: 'gh-pages',
